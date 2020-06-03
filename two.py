4091be35e38886da6f5c729a3db81ef0380c1a3d
root <lzckira@hotmail.com>
2020-06-02 21:30:15 -0700

    re-tarred

--- two.py
+++ two.py
@@ -8,13 +8,13 @@ temp = []
 fileName = "log2"
 with open(fileName, 'r') as inputFile:
 	for line in inputFile:
-		if (re.search(': connect from unknown',line) and re.search('unknown',line)):
+		if (re.search(': connect from unknown',line)):
 			temp = re.split('n|\r',line)
 			unknown.append(temp[-2])
 		elif (re.search(': connect from ',line)):
 			temp = re.split('\[|\r',line)
 			known.append(temp[-2])
 	print("Total Known connection: " + str(len(Counter(known))) + " - [" + Counter(known).most_common(1)[0][0] + " accounts for " + str(Counter(known).most_common(1)[0][1]) + " connections")
-	print("Total Unknown connection: " + str(len(Counter(unknown))) + " - " + Counter(unknown).most_common(1)[0][0] + " accounts for " + str(Counter(unknown).most_common(1)[0][1]) + " connections")
+	print("Total Unknown connection: " + str(len(Counter(unknown).most_common())) + " - " + Counter(unknown).most_common(1)[0][0] + " accounts for " + str(Counter(unknown).most_common(1)[0][1]) + " connections")
 
 inputFile.close()
